7cfd8ba1b4768d3dd49436ca749e0f1a536ef3f9
Arkadiusz Rutkowski <arkadiusz.rutkowsk90@gmail.com>
2019-08-22 19:54:14 +0200

    Revert "Revert "Remove logic of generate xls (#4)""
    This reverts commit 2a5f3fe6ce51f42c57d4650353b3ec34fec80cc1.

--- ihg/src/main/java/com/wruzjan/ihg/utils/Utils.java
+++ ihg/src/main/java/com/wruzjan/ihg/utils/Utils.java
@@ -45,29 +45,11 @@ public class Utils {
     public static final String SIEMIANOWICE_PDF_SZYMON
             = Environment.getExternalStorageDirectory().toString() + "/IHG/templates/form1_szymon.pdf";
 
-    public static final String SIEMIANOWICE_XLS_MACIEJ
-            = Environment.getExternalStorageDirectory().toString() + "/IHG/templates/form1_maciej.xls";
-
-    public static final String SIEMIANOWICE_XLS_SZYMON
-            = Environment.getExternalStorageDirectory().toString() + "/IHG/templates/form1_szymon.xls";
-
-    public static final String PADEREWSKIEGO_XLS_MACIEJ
-            = Environment.getExternalStorageDirectory().toString() + "/IHG/templates/form2_maciej.xls";
-
-    public static final String PADEREWSKIEGO_XLS_SZYMON
-            = Environment.getExternalStorageDirectory().toString() + "/IHG/templates/form2_szymon.xls";
-
     public static final String PADEREWSKIEGO_PDF_MACIEJ
             = Environment.getExternalStorageDirectory().toString() + "/IHG/templates/form2_maciej.pdf";
 
     public static final String PADEREWSKIEGO_PDF_SZYMON
             = Environment.getExternalStorageDirectory().toString() + "/IHG/templates/form2_szymon.pdf";
-
-    public static final String NEW_PADEREWSKIEGO_XLS_MACIEJ
-            = Environment.getExternalStorageDirectory().toString() + "/IHG/templates/form3_maciej.xls";
-
-    public static final String NEW_PADEREWSKIEGO_XLS_SZYMON
-            = Environment.getExternalStorageDirectory().toString() + "/IHG/templates/form3_szymon.xls";
 
     public static final String NEW_PADEREWSKIEGO_PDF_MACIEJ
             = Environment.getExternalStorageDirectory().toString() + "/IHG/templates/form3_maciej.pdf";
