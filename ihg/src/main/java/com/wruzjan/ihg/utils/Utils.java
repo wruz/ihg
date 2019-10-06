49479d2190988bd08ba15e253df454d026a3ecdb
Arkadiusz Rutkowski <arkadiusz.rutkowsk90@gmail.com>
2019-10-06 13:35:42 +0200

    Moved company address to choose worker screen - Siemanowice

--- ihg/src/main/java/com/wruzjan/ihg/utils/Utils.java
+++ ihg/src/main/java/com/wruzjan/ihg/utils/Utils.java
@@ -95,6 +95,8 @@ public class Utils {
 
     public static final String PRINTER_POSITION = "printerPosition";
 
+    public static final String PREF_SIEMANOWICE_COMPANY_ADDRESS = "PREF_SIEMANOWICE_COMPANY_ADDRESS";
+
 //    moja "00:22:58:02:29:0C"
 //    szymon "00:22:58:02:2C:D9"
 //    public static final String PRINTER_MAC_ADDRESS = "00:22:58:02:2C:D9";
