a1260689130cb80a450c77988f88b9575cf1cd16
Arkadiusz Rutkowski <arkadiusz.rutkowsk90@gmail.com>
2020-01-03 11:10:17 +0100

    Manual editing MAC address

--- ihg/src/main/java/com/wruzjan/ihg/PrinterSettingActivity.java
+++ ihg/src/main/java/com/wruzjan/ihg/PrinterSettingActivity.java
@@ -3,42 +3,48 @@ package com.wruzjan.ihg;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.wruzjan.ihg.utils.StringUtils;
 import com.wruzjan.ihg.utils.Utils;
 
 public class PrinterSettingActivity extends Activity {
+
+    private static final String DEFAULT_PRINTER_VALUE = "00:22:58:02:29:0C";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_printer);
 
-        Spinner spinner = (Spinner) findViewById(R.id.printers_spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.printers, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        EditText printerEditText = findViewById(R.id.printers_edit);
 
         //get data from last entry and fill form
         SharedPreferences settings = getSharedPreferences(Utils.PREFS_NAME, 0);
-        spinner.setSelection(settings.getInt(Utils.PRINTER_POSITION, 0));
+        printerEditText.setText(settings.getString(Utils.PRINTER_MAC, DEFAULT_PRINTER_VALUE));
     }
 
     public void selectPrinter(View view) {
         //get selected worker
-        Spinner printersSpinner = (Spinner) findViewById(R.id.printers_spinner);
-        String printer = (String) printersSpinner.getSelectedItem();
+        EditText printerEditText = findViewById(R.id.printers_edit);
+        String printer = printerEditText.getText().toString();
 
-        SharedPreferences settings = getSharedPreferences(Utils.PREFS_NAME, 0);
-        SharedPreferences.Editor editor = settings.edit();
-        editor.putString(Utils.PRINTER_MAC, printer);
-        editor.putInt(Utils.PRINTER_POSITION, printersSpinner.getSelectedItemPosition());
-        editor.commit();
-        finish();
+        if (StringUtils.isValidMacAddress(printer)) {
+            SharedPreferences settings = getSharedPreferences(Utils.PREFS_NAME, 0);
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putString(Utils.PRINTER_MAC, printer);
+
+            // remove obsolete settings param
+            if (settings.contains(Utils.PRINTER_POSITION)) {
+                editor.remove(Utils.PRINTER_POSITION);
+            }
+
+            editor.commit();
+            finish();
+        } else {
+            Toast.makeText(this, getString(R.string.incorrect_printer_mac_address), Toast.LENGTH_SHORT).show();
+        }
     }
-
 }
