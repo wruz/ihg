0ffc24c30afdf05503864c282aac4124cfe8cd67
DESKTOP-8KO5G74\arkad <arkadiusz.rutkowski90@gmail.com>
2021-01-06 12:17:46 +0100

    CSV reports from dates range

--- app/src/main/java/com/wruzjan/ihg/GenerateDailyReportDialog.java
+++ app/src/main/java/com/wruzjan/ihg/GenerateDailyReportDialog.java
@@ -1,24 +1,23 @@
 package com.wruzjan.ihg;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.View;
+import android.os.Message;
 import android.widget.DatePicker;
-
-import java.util.Calendar;
-import java.util.Date;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class GenerateDailyReportDialog extends DialogFragment {
 
     private static final String ARG_CITY = "ARG_CITY";
 
-    private DatePicker datePicker;
     private City city;
 
     @Nullable
@@ -35,33 +34,48 @@ public class GenerateDailyReportDialog extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        View rootView = View.inflate(requireContext(), R.layout.fragment_generate_daily_report, null);
-
         city = (City) getArguments().getSerializable(ARG_CITY);
 
-        AlertDialog alertDialog = new AlertDialog.Builder(requireActivity())
-                .setTitle(city.toString())
-                .setView(rootView)
-                .setNegativeButton(R.string.generate_daily_report_cancel, new DialogInterface.OnClickListener() {
+        DatePickerDialog pickerDialog = new DatePickerDialog(
+                requireContext(),
+                0,
+                new DatePickerDialog.OnDateSetListener() {
                     @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                })
-                .setPositiveButton(R.string.generate_daily_report, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                         Calendar calendar = Calendar.getInstance();
-                        calendar.set(datePicker.getYear(), datePicker.getMonth(), datePicker.getDayOfMonth());
+                        calendar.set(view.getYear(), view.getMonth(), view.getDayOfMonth());
                         if (listener != null) {
                             listener.onReportGenerate(city, calendar.getTime());
                         }
                     }
-                })
-                .create();
-        datePicker = rootView.findViewById(R.id.date_picker);
-        datePicker.setMaxDate(System.currentTimeMillis());
-        return alertDialog;
+                }, -1, -1, -1);
+
+        pickerDialog.setButton(DialogInterface.BUTTON_NEGATIVE, getString(R.string.generate_daily_report_cancel), Message.obtain());
+        pickerDialog.setButton(DialogInterface.BUTTON_POSITIVE, getString(R.string.generate_daily_report), Message.obtain());
+        pickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+
+//        AlertDialog alertDialog = new AlertDialog.Builder(requireActivity())
+//                .setTitle(city.toString())
+//                .setView(rootView)
+//                .setNegativeButton(R.string.generate_daily_report_cancel, new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        dialog.dismiss();
+//                    }
+//                })
+//                .setPositiveButton(R.string.generate_daily_report, new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        Calendar calendar = Calendar.getInstance();
+//                        calendar.set(datePicker.getYear(), datePicker.getMonth(), datePicker.getDayOfMonth());
+//                        if (listener != null) {
+//                            listener.onReportGenerate(city, calendar.getTime());
+//                        }
+//                    }
+//                })
+//                .create();
+//        datePicker = rootView.findViewById(R.id.date_picker);
+        return pickerDialog;
     }
 
     @Override
